4e126de36fdf8667060f0df70ddaa8cc151dadd4
U-hades\dlane <dlane@rackspace.com>
2017-01-08 22:20:10 -0600

    Bundles WIP

---
+++ ShaderLoader.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace KerbPaint.Shaders
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class KerbPaintShaderLoader : MonoBehaviour
+    {
+        private static bool _loaded;
+
+        private static string _bundlePath;
+
+        public static Shader paintShader;
+
+        public static Dictionary<string, Shader> LoadedShaders = new Dictionary<string, Shader>();
+
+        public string BundlePath
+        {
+            get
+            {
+                switch (Application.platform)
+                {
+                    case RuntimePlatform.OSXPlayer:
+                        return _bundlePath + Path.DirectorySeparatorChar +
+                               "shaders_macosx.bundle";
+                    case RuntimePlatform.WindowsPlayer:
+                        return _bundlePath + Path.DirectorySeparatorChar +
+                               "shaders_windows.bundle";
+                    case RuntimePlatform.LinuxPlayer:
+                        return _bundlePath + Path.DirectorySeparatorChar +
+                               "shaders_macosx.bundle";
+                    default:
+                        return _bundlePath + Path.DirectorySeparatorChar +
+                               "shaders_windows.bundle";
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            _bundlePath = KSPUtil.ApplicationRootPath + "GameData" +
+                                                    Path.DirectorySeparatorChar +
+                                                    "KerbPaint" + Path.DirectorySeparatorChar + "AssetBundles";
+        }
+        private void Start()
+        {
+            if (!_loaded)
+            {
+                Debug.Log("[KerbPaint] start bundle load process");
+                StartCoroutine(LoadBundleAssets());
+                _loaded = true;
+            }
+        }
+
+        private AssetBundle shaderBundle;
+
+        private IEnumerator LoadBundleAssets()
+        {
+            Debug.Log("[KerbPaint] Loading bundle data");
+
+            shaderBundle = AssetBundle.LoadFromFile(BundlePath);
+
+            if (shaderBundle != null)
+            {
+                var shaders = shaderBundle.LoadAllAssets<Shader>();
+
+                foreach (var shader in shaders)
+                {
+                    Debug.Log($"[KerbPaint] Shader \"{shader.name}\" loaded. Shader supported? {shader.isSupported}");
+                    LoadedShaders.Add(shader.name, shader);
+                }
+                yield return null;
+                Debug.Log("[KerbPaint] unloading bundle");
+                shaderBundle.Unload(false); // unload the raw asset bundle
+            }
+            else
+            {
+                Debug.Log("[KerbPaint] Error: Found no asset bundle to load");
+            }
+        }
+
+        public static Shader LoadShader(string name)
+        {
+            if (LoadedShaders.ContainsKey(name))
+                return LoadedShaders[name];
+
+            Debug.Log("Shader " + name + " not found!");
+            return Shader.Find("Hidden/InternalErrorShader");
+        }
+
+    }
+}
